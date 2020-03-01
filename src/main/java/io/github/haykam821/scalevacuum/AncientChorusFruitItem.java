9ee57fb0d9e63717f5313f4d3e00ac058e56c797
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-03-01 02:30:50 -0500

    Allow teleportation to and from the dimension

---
+++ src/main/java/io/github/haykam821/scalevacuum/AncientChorusFruitItem.java
@@ -0,0 +1,35 @@
+package io.github.haykam821.scalevacuum;
+
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+
+class AncientChorusFruitItem extends Item {
+	public AncientChorusFruitItem(Settings settings) {
+		super(settings);
+	}
+
+	public boolean hasEnchantmentGlint(ItemStack itemStack) {
+		return true;
+	}
+
+	@Override
+	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity entity) {
+		ItemStack newStack = super.finishUsing(stack, world, entity);
+
+		entity.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 80));
+
+		if (entity.dimension == Main.SCALE_VACUUM) {
+			FabricDimensions.teleport(entity, DimensionType.OVERWORLD, Main.OVERWORLD_SURFACE_PLACER);
+		} else {
+			FabricDimensions.teleport(entity, Main.SCALE_VACUUM);
+		}
+
+		return newStack;
+	}
+}
--- src/main/java/io/github/haykam821/scalevacuum/Main.java
+++ src/main/java/io/github/haykam821/scalevacuum/Main.java
@@ -1,10 +1,18 @@
 package io.github.haykam821.scalevacuum;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.dimension.v1.EntityPlacer;
 import net.fabricmc.fabric.api.dimension.v1.FabricDimensionType;
 import net.minecraft.block.pattern.BlockPattern;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FoodComponents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.Heightmap;
@@ -13,6 +21,26 @@ import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 
 public class Main implements ModInitializer {
+	public static EntityPlacer OVERWORLD_SURFACE_PLACER = (Entity entity, ServerWorld destination, Direction portalDir, double horizontalOffset, double verticalOffset) -> {
+		BlockPos spawnPos = destination.getSpawnPos();
+		if (entity instanceof PlayerEntity) {
+			spawnPos = ((PlayerEntity) entity).getSpawnPosition();
+		}
+
+		return new BlockPattern.TeleportTarget(
+			new Vec3d(destination.getTopPosition(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, spawnPos)),
+			entity.getVelocity(),
+			(int) entity.yaw
+		);
+	};
+	public static EntityPlacer SCALE_VACUUM_PLACER = (Entity entity, ServerWorld destination, Direction portalDir, double horizontalOffset, double verticalOffset) -> {
+		return new BlockPattern.TeleportTarget(
+			new Vec3d(destination.getForcedSpawnPoint()),
+			entity.getVelocity(),
+			(int) entity.yaw
+		);
+	};
+
 	public static final Feature<DefaultFeatureConfig> SCALE_PLATFORM = Registry.register(
 		Registry.FEATURE,
 		new Identifier("scalevacuum", "scale_platform"), 
@@ -21,21 +49,19 @@ public class Main implements ModInitializer {
 
 	public static final Biome SCALE_VACUUM_VOID = new ScaleVacuumVoidBiome();
 	public static FabricDimensionType SCALE_VACUUM = FabricDimensionType.builder()
-			.defaultPlacer((oldEntity, destinationWorld, portalDir, horizontalOffset, verticalOffset) -> {
-				return new BlockPattern.TeleportTarget(
-						new Vec3d(destinationWorld.getTopPosition(
-							Heightmap.Type.WORLD_SURFACE,
-							BlockPos.ORIGIN
-						)).multiply(0.1),
-						oldEntity.getVelocity(),
-						(int) oldEntity.yaw
-				);
-			})
+			.defaultPlacer(SCALE_VACUUM_PLACER)
 			.factory(ScaleVacuum::new)
 			.buildAndRegister(new Identifier("scalevacuum", "scale_vacuum"));
+
+  	public static final Item ANCIENT_CHORUS_FRUIT = new AncientChorusFruitItem(
+		new Item.Settings()
+		.group(ItemGroup.MATERIALS)
+		.food(FoodComponents.CHORUS_FRUIT)
+	);
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.BIOME, new Identifier("scalevacuum", "scale_vacuum_void"), SCALE_VACUUM_VOID);
+		Registry.register(Registry.ITEM, new Identifier("scalevacuum", "ancient_chorus_fruit"), ANCIENT_CHORUS_FRUIT);
 	}
 }
