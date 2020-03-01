b796d701e8aef165458e5b53ef7ac9a5141143b9
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-03-01 13:47:12 -0500

    Reduce player health while in the dimension

---
+++ src/main/java/io/github/haykam821/scalevacuum/mixin/LivingEntityMixin.java
@@ -0,0 +1,35 @@
+package io.github.haykam821.scalevacuum.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import io.github.haykam821.scalevacuum.Main;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+	@Shadow
+	abstract float getMaximumHealth();
+
+	public LivingEntityMixin(EntityType<?> type, World world) {
+		super(type, world);
+	}
+
+	@Inject(method = "getMaximumHealth", at = @At("RETURN"), cancellable = true, remap = false)
+	void getMaximumHealthInScaleVacuum(CallbackInfoReturnable<Float> ci) {
+		if ((Object) this instanceof PlayerEntity && this.dimension == Main.SCALE_VACUUM) {
+			float originalHealth = ci.getReturnValue();
+			float unbreathableHealth = (float) Math.ceil((float) 1 / 5 * 20);
+			float breathableHealth = originalHealth - unbreathableHealth;
+
+			ci.setReturnValue(breathableHealth);
+		}
+	}
+}
