c3de9cbdc7a75e8a7e08a5e043db05f02fcb609d
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-01-24 17:29:32 -0500

    Use the end sky properties for the scale vacuum dimension type

--- src/main/java/io/github/haykam821/scalevacuum/ClientMain.java
+++ src/main/java/io/github/haykam821/scalevacuum/ClientMain.java
@@ -2,10 +2,12 @@ package io.github.haykam821.scalevacuum;
 
 import io.github.haykam821.scalevacuum.block.ScaleVacuumBlocks;
 import io.github.haykam821.scalevacuum.item.ScaleVacuumItems;
+import io.github.haykam821.scalevacuum.mixin.SkyPropertiesAccessor;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.SkyProperties;
 
 public class ClientMain implements ClientModInitializer {
 	@Override
@@ -15,5 +17,8 @@ public class ClientMain implements ClientModInitializer {
 		}, ScaleVacuumItems.ENDER_PURIFIER);
 
 		BlockRenderLayerMap.INSTANCE.putBlock(ScaleVacuumBlocks.DRAGON_FIRE.getBlock(), RenderLayer.getCutout());
+
+		// Change sky to end
+		SkyPropertiesAccessor.getPropertiesMap().put(Main.SCALE_VACUUM_ID, new SkyProperties.End());
 	}
 }
